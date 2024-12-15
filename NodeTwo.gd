72e6190e327aad429c297851c9cca33c77b1b5a1
FrasherGray <59066601+FrasherGray@users.noreply.github.com>
2024-12-15 15:19:15 -0500

    Update NodeTwo.gd
    rpc bit is irrelevant

--- NodeTwo.gd
+++ NodeTwo.gd
@@ -5,7 +5,6 @@ signal beginNextTurn
 @onready var node1 = get_parent().get_node("NodeOne")
 @onready var node3 = get_parent().get_node("NodeThree")
 
-@rpc("any_peer", "call_local", "reliable")
 func beginNewTurn() -> void:
 		node3.takeTurn()
 
